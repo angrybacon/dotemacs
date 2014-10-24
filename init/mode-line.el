44d51d3f1e82230a698f02e57d673df2786359e4
Mathieu Marques <mathieumarques78@gmail.com>
2014-10-24 20:50:29 +0200

    Added mode-line.el

---
+++ init/mode-line.el
@@ -0,0 +1,147 @@
+;; Nyan mode (https://github.com/TeMPOraL/nyan-mode/)
+(add-to-list 'load-path "~/.emacs.d/packages/nyan-mode/")
+;; (require 'nyan-mode)
+;; (nyan-mode)
+
+;; Mode line setup (http://amitp.blogspot.fr/2011/08/emacs-custom-mode-line.html)
+(setq-default mode-line-format
+              ;; Position, including warning for 80 columns
+              '((:propertize "%4l:" face mode-line-position-face)
+                (:eval (propertize "%3c" 'face
+                                   (if (>= (current-column) 80)
+                                       'mode-line-80col-face
+                                     'mode-line-position-face)))
+
+                ;; emacsclient [default -- keep?]
+                mode-line-client
+                "  "
+
+                ;; Read-only or modified status
+                (:eval (cond (buffer-read-only
+                              (propertize " RO " 'face 'mode-line-read-only-face))
+                             ((buffer-modified-p)
+                              (propertize " ** " 'face 'mode-line-modified-face))
+                             ;; (t "      ")))
+                             (t "    ")))
+                "    "
+
+                ;; Directory and buffer/file name
+                (:propertize (:eval (shorten-directory default-directory 30))
+                             face mode-line-folder-face)
+                (:propertize "%b"
+                             face mode-line-filename-face)
+
+                ;; narrow [default -- keep?]
+                " %n "
+
+                ;; Mode indicators: vc, recursive edit, major mode, minor modes, process, global
+                (vc-mode vc-mode)
+                "  %["
+                (:propertize mode-name face mode-line-mode-face)
+                "%] "
+                (:eval (propertize (format-mode-line minor-mode-alist)
+                                   'face 'mode-line-minor-mode-face))
+                (:propertize mode-line-process
+                             face mode-line-process-face)
+                (global-mode-string global-mode-string)
+                "    "
+
+                ;; nyan-mode uses nyan cat as an alternative to %p
+                (:eval (when nyan-mode (list (nyan-create))))
+                ))
+
+;; Helper function
+(defun shorten-directory (dir max-length)
+  "Show up to 'max-length' characters of a directory name 'dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
+;; Extra mode line faces
+(make-face 'mode-line-read-only-face)
+(make-face 'mode-line-modified-face)
+(make-face 'mode-line-folder-face)
+(make-face 'mode-line-filename-face)
+(make-face 'mode-line-position-face)
+(make-face 'mode-line-mode-face)
+(make-face 'mode-line-minor-mode-face)
+(make-face 'mode-line-process-face)
+(make-face 'mode-line-80col-face)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#586E75"
+                    :background "#073642"
+                    :inverse-video nil
+                    :underline nil
+                    :overline nil
+                    :box '(:line-width 1 :color "#073642" :style nil)
+                    )
+
+(set-face-attribute 'mode-line-inactive nil
+                    ;; :foreground "gray80" :background "gray40"
+                    :inverse-video nil
+                    :underline nil
+                    :overline nil
+                    :box '(:line-width 1 :color "#073642" :style nil)
+                    )
+
+(set-face-attribute 'mode-line-read-only-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#CB4B16"
+                    :background "#268BD2"
+                    :weight 'bold
+                    )
+
+
+(set-face-attribute 'mode-line-modified-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#268BD2"
+                    :background "#CB4B16"
+                    :weight 'bold
+                    )
+
+
+(set-face-attribute 'mode-line-folder-face nil
+                    :inherit 'mode-line-face
+                    )
+
+(set-face-attribute 'mode-line-filename-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#93A1A1"
+                    :weight 'bold
+                    )
+
+(set-face-attribute 'mode-line-position-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#93A1A1"
+                    :weight 'bold
+                    )
+
+(set-face-attribute 'mode-line-mode-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#93A1A1"
+                    :weight 'bold
+                    )
+
+
+(set-face-attribute 'mode-line-minor-mode-face nil
+                    :inherit 'mode-line-face
+                    )
+
+(set-face-attribute 'mode-line-process-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#718C00"
+                    )
+
+
+(set-face-attribute 'mode-line-80col-face nil
+                    :inherit 'mode-line-position-face
+                    :foreground "#CB4B16"
+                    )
