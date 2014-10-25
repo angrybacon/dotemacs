2a48d79c91124ef29a8392fef82ddea4791a8349
Mathieu Marques <mathieumarques78@gmail.com>
2014-10-25 18:32:26 +0200

    Reduced the maxmimum size for directory string in the mode line

--- init-mode-line.el
+++ init-mode-line.el
@@ -44,7 +44,7 @@
                 "    "
 
                 ;; Directory and buffer/file name
-                (:propertize (:eval (shorten-directory default-directory 30))
+                (:propertize (:eval (shorten-directory default-directory 20))
                              face mode-line-folder-face)
                 (:propertize "%b"
                              face mode-line-filename-face)
