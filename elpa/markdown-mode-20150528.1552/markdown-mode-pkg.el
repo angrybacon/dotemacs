f1a3b6b253c919d9738d4588f3d681e5ebc9106b
Mathieu Marques <mathieumarques78@gmail.com>
2015-06-06 12:48:13 +0200

    Update packages

--- elpa/markdown-mode-20150528.1552/markdown-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "markdown-mode" "20150528.1552" "Emacs Major mode for Markdown-formatted text files" 'nil :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
