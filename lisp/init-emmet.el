9808ed88407e189a266d0ef0943f245f6f3cd4dc
Mathieu Marques <mathieumarques78@gmail.com>
2015-06-10 12:27:19 +0200

    Fix emmet-mode binds overwriting custom binds

--- lisp/init-emmet.el
+++ lisp/init-emmet.el
@@ -12,7 +12,10 @@
    emmet-move-cursor-between-quotes t)
   (add-hook 'css-mode-hook 'emmet-mode)
   (add-hook 'sgml-mode-hook 'emmet-mode)
-  (add-hook 'web-mode-hook 'emmet-mode))
+  (add-hook 'web-mode-hook 'emmet-mode)
+  :config
+  (define-key emmet-mode-keymap (kbd "C-M-<left>") nil)
+  (define-key emmet-mode-keymap (kbd "C-M-<right>") nil))
 
 
 ;;─────────────────────────────────────────────────────────────────────────────
