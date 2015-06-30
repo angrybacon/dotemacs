c4c8818a82af0bc9be9ae4597e625d5599dc99ea
Mathieu Marques <mathieumarques78@gmail.com>
2015-06-30 19:10:40 +0200

    Fix battery load string color

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -22,74 +22,56 @@
    powerline-default-separator 'wave)
 
   ;; Define new faces for elements on an active powerline
+  (defface my/pl-battery-face '((t (:inherit powerline-active1)))
+    "Face used for the battery load.")
   (defface my/pl-battery-charging-face '((t (:inherit powerline-active1)))
-    "Face used for the battery load when charging."
-    :group 'powerline)
+    "Face used for the battery load when charging.")
   (defface my/pl-battery-discharging-face '((t (:inherit powerline-active1)))
-    "Face used for the battery load when discharging."
-    :group 'powerline)
+    "Face used for the battery load when discharging.")
   (defface my/pl-buffer-name-face '((t (:inherit powerline-active2)))
-    "Face used for the buffer name."
-    :group 'powerline)
+    "Face used for the buffer name.")
   (defface my/pl-buffer-status-face '((t (:inherit powerline-active1)))
-    "Face used for the buffer status."
-    :group 'powerline)
+    "Face used for the buffer status.")
   (defface my/pl-major-mode-face '((t (:inherit nil)))
-    "Face used for the major mode."
-    :group 'powerline)
+    "Face used for the major mode.")
   (defface my/pl-line-number-face '((t (:inherit powerline-active2)))
-    "Face used for the line number."
-    :group 'powerline)
+    "Face used for the line number.")
   (defface my/pl-line-separator-face '((t (:inherit powerline-active2)))
-    "Face used for the line separator."
-    :group 'powerline)
+    "Face used for the line separator.")
   (defface my/pl-project-delimiter-face '((t (:inherit powerline-active1)))
-    "Face used for the project delimiters."
-    :group 'powerline)
+    "Face used for the project delimiters.")
   (defface my/pl-project-name-face '((t (:inherit powerline-active1)))
-    "Face used for the project name."
-    :group 'powerline)
+    "Face used for the project name.")
   (defface my/pl-time-face '((t (:inherit nil)))
-    "Face used for the time."
-    :group 'powerline)
+    "Face used for the time.")
   (defface my/pl-vc-branch-face '((t (:inherit powerline-active1)))
-    "Face used for the version control branch."
-    :group 'powerline)
+    "Face used for the version control branch.")
 
   ;; Define new faces for elements on an inactive powerline
+  (defface my/pl-battery-inactive-face '((t (:inherit powerline-inactive1)))
+    "Face used for the battery load, in an inactive powerline.")
   (defface my/pl-battery-charging-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the battery load when charging, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the battery load when charging, in an inactive powerline.")
   (defface my/pl-battery-discharging-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the battery load when discharging, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the battery load when discharging, in an inactive powerline.")
   (defface my/pl-buffer-name-inactive-face '((t (:inherit powerline-inactive2)))
-    "Face used for the buffer name, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the buffer name, in an inactive powerline.")
   (defface my/pl-buffer-status-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the buffer status, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the buffer status, in an inactive powerline.")
   (defface my/pl-major-mode-inactive-face '((t (:inherit nil)))
-    "Face used for the major mode, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the major mode, in an inactive powerline.")
   (defface my/pl-line-number-inactive-face '((t (:inherit powerline-inactive2)))
-    "Face used for the line number, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the line number, in an inactive powerline.")
   (defface my/pl-line-separator-inactive-face '((t (:inherit powerline-inactive2)))
-    "Face used for the line separator, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the line separator, in an inactive powerline.")
   (defface my/pl-project-delimiter-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the project delimiters, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the project delimiters, in an inactive powerline.")
   (defface my/pl-project-name-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the project name, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the project name, in an inactive powerline.")
   (defface my/pl-time-inactive-face '((t (:inherit nil)))
-    "Face used for the time, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the time, in an inactive powerline.")
   (defface my/pl-vc-branch-inactive-face '((t (:inherit powerline-inactive1)))
-    "Face used for the version control branch, in an inactive powerline."
-    :group 'powerline)
+    "Face used for the version control branch, in an inactive powerline.")
 
   ;; Define the mode-line format
   (setq-default
@@ -105,10 +87,7 @@
              (mode-line-face (if active 'mode-line 'mode-line-inactive))
              (mode-line-major-face (if active 'powerline-active1 'powerline-inactive1))
              (mode-line-minor-face (if active 'powerline-active2 'powerline-inactive2))
-             (battery-charging-face
-              (if active 'my/pl-battery-charging-face 'my/pl-battery-charging-inactive-face))
-             (battery-discharging-face
-              (if active 'my/pl-battery-discharging-face 'my/pl-battery-discharging-inactive-face))
+             (battery-face (if active 'my/pl-battery-face 'my/pl-battery-inactive-face))
              (buffer-name-face (if active 'my/pl-buffer-name-face 'my/pl-buffer-name-inactive-face))
              (buffer-status-face (if active 'my/pl-buffer-status-face 'my/pl-buffer-status-inactive-face))
              (major-mode-face (if active 'my/pl-major-mode-face 'my/pl-major-mode-inactive-face))
@@ -137,7 +116,7 @@
                    (powerline-raw (projectile-project-name) project-name-face)
 
                    ;; TODO: Remove version control backend
-                   (powerline-raw (powerline-vc vc-branch-face) vc-branch-face)
+                   (powerline-vc vc-branch-face)
 
                    (powerline-raw "]" project-delimiter-face)
                    (powerline-raw "%+" buffer-status-face 'l)
@@ -158,7 +137,8 @@
                    (powerline-raw " " mode-line-major-face)
 
                    ;; FIXME: I cannot add a percent character with `battery-mode-line-format'
-                   (powerline-raw (concat battery-mode-line-string "%%") battery-discharging-face 'r)
+                   ;; (powerline-raw (concat battery-mode-line-string "%%") battery-discharging-face 'r)
+                   (powerline-raw (concat battery-mode-line-string "%%") battery-face 'r)
 
                    (funcall separator-right mode-line-major-face mode-line-face)
                    (powerline-raw " " nil)
@@ -171,6 +151,14 @@
 
   :config
 
+  ;; TODO: This should set faces for the inactive powerline as well
+  ;; Colorize the battery load string on update
+  (defadvice battery-update (before my/pl-colorize-battery-advice activate)
+    "Colorize the battery load string depending on its status (dis/charging)."
+    (if (string-equal "AC" (cdr (assoc 76 (funcall battery-status-function))))
+        (copy-face 'my/pl-battery-charging-face 'my/pl-battery-face)
+      (copy-face 'my/pl-battery-discharging-face 'my/pl-battery-face)))
+
   ;; FIXME: The mode-line is slightly thinner when using Helm. How to make Helm use powerline?
 
   ;; Customize fonts within a mode-line
@@ -181,6 +169,7 @@
   ;; Customize faces for an active mode-line
   (set-face-attribute 'mode-line nil :foreground zenburn/green+1 :box `(:line-width 2 :color ,zenburn/bg-1))
   (set-face-attribute 'powerline-active2 nil :background zenburn/bg+0)
+  (set-face-attribute 'my/pl-battery-face nil :foreground zenburn/bg+3)
   (set-face-attribute 'my/pl-battery-charging-face nil :foreground zenburn/green)
   (set-face-attribute 'my/pl-battery-discharging-face nil :foreground zenburn/red)
   (set-face-attribute 'my/pl-buffer-name-face nil :foreground zenburn/green+1)
@@ -197,7 +186,8 @@
   (set-face-attribute 'mode-line-inactive nil :foreground zenburn/fg-1
                       :box `(:line-width 2 :color ,zenburn/bg-0) :slant 'unspecified)
   (set-face-attribute 'powerline-inactive2 nil :background zenburn/bg+2)
-  (set-face-attribute 'my/pl-battery-charging-inactive-face nil :foreground zenburn/yellow)
+  (set-face-attribute 'my/pl-battery-inactive-face nil :foreground zenburn/bg+3)
+  (set-face-attribute 'my/pl-battery-charging-inactive-face nil :foreground zenburn/bg+3)
   (set-face-attribute 'my/pl-battery-discharging-inactive-face nil :foreground zenburn/bg+3)
   (set-face-attribute 'my/pl-buffer-name-inactive-face nil :foreground zenburn/fg)
   (set-face-attribute 'my/pl-buffer-status-inactive-face nil :foreground zenburn/red)
