6defd9407397837ecc911445379e17f6c274ac89
Mathieu Marques <mathieumarques78@gmail.com>
2015-07-07 09:49:01 +0200

    Update packages

--- elpa/dash-20150701.20/dash-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21908 62856
-;;;;;;  541900 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- elpa/dash-20150701.20/dash-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dash" "20150701.20" "A modern list library for Emacs" 'nil :keywords '("lists"))
