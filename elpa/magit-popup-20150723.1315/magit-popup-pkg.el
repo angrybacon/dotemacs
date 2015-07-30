f2f3fc30ea839fc68307cb7141953fb6c887540a
Mathieu Marques <mathieumarques78@gmail.com>
2015-07-30 11:09:40 +0200

    Update packages

--- elpa/magit-popup-20150723.1315/magit-popup-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "magit-popup" "20150723.1315" "Define prefix-infix-suffix command combos"
-  '((emacs "24.4")
-    (dash "2.10.0"))
-  :url "https://github.com/magit/magit" :keywords
-  '("bindings"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/magit-popup-20150728.2305/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (21944 40295 710736 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
