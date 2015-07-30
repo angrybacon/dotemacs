7c3ec042b43deea49abceabe0b0a01071f101cf4
Mathieu Marques <mathieumarques78@gmail.com>
2015-07-30 11:10:13 +0200

    Enable completion for some modes only

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -10,7 +10,10 @@
   (setq
    company-minimum-prefix-length 1
    company-idle-delay 0.1)
-  (add-hook 'after-init-hook 'global-company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
+  (add-hook 'html-mode-hook 'company-mode)
+  (add-hook 'python-mode-hook 'company-mode)
+  (add-hook 'scss-mode-hook 'company-mode)
   :config
   (set-face-attribute 'company-tooltip-common nil :inherit 'company-tooltip)
   (set-face-attribute 'company-tooltip-common-selection nil :inherit 'company-tooltip-selection))
