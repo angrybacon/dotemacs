93ab3dce0ff23310fbdaf80b50fe81905ac9aa26
Mathieu Marques <mathieumarques78@gmail.com>
2016-01-12 12:56:53 +0100

    Change foreground color for function names

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -27,6 +27,7 @@
 (defvar zenburn/bg)
 (defvar zenburn/bg-1)
 (defvar zenburn/bg-2)
+(defvar zenburn/blue)
 (defvar zenburn/fg-1)
 (defvar zenburn/green+2)
 (defvar zenburn/green)
@@ -41,6 +42,7 @@
 ;; Website: https://github.com/bbatsov/zenburn-emacs
 (use-package zenburn-theme
   :ensure t
+
   :init
   (load-theme 'zenburn t))
 
@@ -62,9 +64,9 @@
 (set-face-attribute 'font-lock-constant-face nil :foreground zenburn/green-1)
 (set-face-attribute 'font-lock-comment-face nil :foreground zenburn/fg-1 :italic t)
 (set-face-attribute 'font-lock-comment-delimiter-face nil :italic t)
+(set-face-attribute 'font-lock-function-name-face nil :foreground zenburn/blue)
 (set-face-attribute 'fringe nil :background zenburn/bg :foreground zenburn/bg+2)
 (set-face-attribute 'header-line nil :background zenburn/bg+0 :box nil)
-;; (set-face-attribute 'header-line nil :background zenburn/bg+0 :box `(:line-width 4 :color ,zenburn/bg+0))
 (set-face-attribute 'isearch nil :foreground zenburn/green+2 :background zenburn/bg-2)
 (set-face-attribute 'lazy-highlight nil :foreground zenburn/green+2 :background zenburn/bg-1)
 (set-face-attribute 'region nil :foreground zenburn/green)
