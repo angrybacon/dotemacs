64a7f5dacf4b9a8a83f80f99267dfe984cd723f0
Mathieu Marques <mathieumarques78@gmail.com>
2016-04-28 10:40:38 +0200

    Update selected.el

--- selected.el
+++ selected.el
@@ -5,7 +5,7 @@
 
 ;; Author: Erik Sjöstrand
 ;; URL: http://github.com/Kungsgeten/selected.el
-;; Version: 1.00
+;; Version: 1.01
 ;; Keywords: convenience
 ;; Package-Requires: ()
 
@@ -25,32 +25,35 @@
 ;; 
 ;; (setq selected-org-mode-map (make-sparse-keymap))
 ;; (define-key selected-org-mode-map (kbd "t") #'org-table-convert-region)
+;;
+;; There's also a global minor mode available: `selected-global-mode' if you
+;; want selected-minor-mode in all buffers.
 
 ;;; Code:
 (defvar selected-keymap (make-sparse-keymap)
   "Keymap for `selected-minor-mode'.  Add keys here that should be active when region is active.")
 
-(define-minor-mode selected-region-active
+(define-minor-mode selected-region-active-mode
   "Meant to activate when region becomes active.  Not intended for the user.  Use `selected-minor-mode'."
   :keymap selected-keymap
-  (when selected-region-active
+  (when selected-region-active-mode
     (let ((major-selected-map
            (intern-soft (concat "selected-" (symbol-name major-mode) "-map"))))
       (if major-selected-map
-          (setf (cdr (assoc 'selected-region-active minor-mode-map-alist))
+          (setf (cdr (assoc 'selected-region-active-mode minor-mode-map-alist))
                 (let ((map (eval major-selected-map)))
                   (set-keymap-parent map selected-keymap)
                   map))
-        (setf (cdr (assoc 'selected-region-active minor-mode-map-alist))
+        (setf (cdr (assoc 'selected-region-active-mode minor-mode-map-alist))
               selected-keymap)))))
 
 (defun selected--on ()
-  (selected-region-active 1))
+  (selected-region-active-mode 1))
 
 (defun selected-off ()
   "Disable bindings in `selected-keymap' temporarily."
   (interactive)
-  (selected-region-active -1))
+  (selected-region-active-mode -1))
 
 ;;;###autoload
 (define-minor-mode selected-minor-mode
@@ -63,6 +66,16 @@
     (remove-hook 'activate-mark-hook #'selected--on)
     (remove-hook 'deactivate-mark-hook #'selected-off)
     (selected-off)))
+
+(defun selected--global-on-p ()
+  "If `selected-global-mode' should activate in a new buffer."
+  (unless (minibufferp)
+    (selected-minor-mode 1)))
+
+;;;###autoload
+(define-globalized-minor-mode selected-global-mode
+  selected-minor-mode
+  selected--global-on-p)
 
 (provide 'selected)
 ;;; selected.el ends here
