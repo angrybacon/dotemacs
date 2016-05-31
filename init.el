a03519a801e721dcce1e6adf793277e024be9d13
Mathieu Marques <mathieumarques78@gmail.com>
2016-05-31 15:07:59 +0200

    Change license

--- init.el
+++ init.el
@@ -5,27 +5,21 @@
 ;; Author: Mathieu Marques <mathieumarques78@gmail.com>
 ;; Created: 16 Oct 2014
 ;; Keywords: abbrev, convenience, faces, maint, outlines, vc
-;; Homepage: https://bitbucket.org/angrybacon/dotemacs
+;; Homepage: https://github.com/angrybacon/dotemacs
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; This program is free software. It comes without any warranty, to the extent
+;; permitted by applicable law. You can redistribute it and/or modify it under the
+;; terms of the Do What The Fuck You Want To Public License, Version 2, as published
+;; by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
 ;;; Commentary:
 
 ;; To install, clone dotemacs/ into ~/.emacs.d/.
 ;;
-;; Following lines load several packages to configure my Emacs experience.
-;; I work quite often with Python, HTML, SCSS and JavaScript code.
+;; Following lines load an Org file and build the configuration code out of it. This
+;; process is known as tangling.
+;; Using this configuration on a daily basis, I work quite often with Python, HTML,
+;; SCSS and JavaScript code.
 ;;
 ;; See README.md for more details.
 
