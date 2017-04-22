fe69d84051104118dddcc20478e0e06b946a7b17
Mathieu Marques <mathieumarques78@gmail.com>
2017-04-22 16:52:08 +0200

    Refactor package initialization to load latest Org

--- init.el
+++ init.el
@@ -21,14 +21,37 @@
 
 ;;; Code:
 
-;; (package-initialize)
+(let ((gc-cons-threshold most-positive-fixnum))
 
-;; Mark `me/project-name' as safe
-(defvar me/project-name nil)
-(put 'me/project-name 'safe-local-variable #'stringp)
+  ;; Mark directory-local variables as safe
+  (defvar me/project-name nil)
+  (put 'me/project-name 'safe-local-variable #'stringp)
 
-;; Tangle configuration
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
+  ;; Set repositories
+  (require 'package)
+  (setq-default
+   load-prefer-newer t
+   package-enable-at-startup nil)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)
 
+  ;; Install dependencies
+  (unless (and (package-installed-p 'delight)
+               (package-installed-p 'use-package))
+    (package-refresh-contents)
+    (package-install 'delight t)
+    (package-install 'use-package t))
+  (setq-default
+   use-package-always-defer t
+   use-package-always-ensure t)
+
+  ;; Use latest Org
+  (use-package org
+    :pin org
+    :ensure org-plus-contrib)
+
+  ;; Tangle configuration
+  (org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
+  (garbage-collect))
 ;;; init.el ends here
