c13d760cc90688eeb5707ddea42f48811558b3c2
Mathieu Marques <mathieumarques78@gmail.com>
2017-05-20 16:35:11 +0200

    Rephrase copyright statement

--- init.el
+++ init.el
@@ -3,21 +3,24 @@
 ;; Copyright (C) 2014 Mathieu Marques
 
 ;; Author: Mathieu Marques <mathieumarques78@gmail.com>
-;; Created: 16 Oct 2014
-;; Keywords: abbrev, convenience, faces, maint, outlines, vc
+;; Created: October 16, 2014
 ;; Homepage: https://github.com/angrybacon/dotemacs
+;; Keywords: abbrev, convenience, faces, maint, outlines, vc
 
-;; This program is free software. It comes without any warranty, to the extent
-;; permitted by applicable law. You can redistribute it and/or modify it under the
-;; terms of the Do What The Fuck You Want To Public License, Version 2, as published
-;; by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+;; This program is free software. You can redistribute it and/or modify it under
+;; the terms of the Do What The Fuck You Want To Public License, version 2 as
+;; published by Sam Hocevar.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.
+;;
+;; You should have received a copy of the Do What The Fuck You Want To Public License
+;; along with this program. If not, see http://www.wtfpl.net/.
 
 ;;; Commentary:
 
-;; Following lines load an Org file and build the configuration code out of it. This
-;; process is known as tangling.
-;;
-;; See README.md for more details.
+;; Following lines load an Org file and build the configuration code out of it.
 
 ;;; Code:
 
