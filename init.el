9a93254f90c278a1e005269b3e68cec86bdba384
Mathieu Marques <mathieumarques78@gmail.com>
2020-11-08 00:14:11 +0100

    Remove unused Org package archive

--- init.el
+++ init.el
@@ -37,7 +37,6 @@
    load-prefer-newer t
    package-enable-at-startup nil)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
 
   ;; Install dependencies
