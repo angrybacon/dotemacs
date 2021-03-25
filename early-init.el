0381a3773d55c7e68d60718c561196a8c7bf270e
Mathieu Marques <mathieumarques78@gmail.com>
2021-03-25 16:23:34 +0100

    chore(theme): widen fringes 3 -> 8

--- early-init.el
+++ early-init.el
@@ -33,10 +33,10 @@
  default-frame-alist
  '((fullscreen . fullboth)
    (horizontal-scroll-bars . nil)       ;; No horizontal scroll-bars
-   (left-fringe . 3)                    ;; Thin left fringe
+   (left-fringe . 8)                    ;; Thin left fringe
    (menu-bar-lines . 0)                 ;; No menu bar
    (right-divider-width . 1)            ;; Thin vertical window divider
-   (right-fringe . 3)                   ;; Thin right fringe
+   (right-fringe . 8)                   ;; Thin right fringe
    (tool-bar-lines . 0)                 ;; No tool bar
    (vertical-scroll-bars . nil)))       ;; No vertical scroll-bars
 
