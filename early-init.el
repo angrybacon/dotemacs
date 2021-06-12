83790b2224655a92e167340205b9c2a7c5bc6752
Mathieu Marques <mathieumarques78@gmail.com>
2021-06-12 13:33:35 +0200

    feature(theme): lighten decorations during initialization

--- early-init.el
+++ early-init.el
@@ -26,13 +26,14 @@
 
 (setq-default
  load-prefer-newer t
+ mode-line-format nil
  package-enable-at-startup nil
  package-native-compile t)
 
 (setq-default
  default-frame-alist
- '((background-color . "#3F3F3F")       ;; Default background color
-   (foreground-color . "#DCDCCC")       ;; Default foreground color
+ '((background-color . "#101010")       ;; Default background color
+   (foreground-color . "#FAFAFA")       ;; Default foreground color
    (fullscreen . maximized)             ;; Maximize the window by default
    (horizontal-scroll-bars . nil)       ;; No horizontal scroll-bars
    (left-fringe . 8)                    ;; Thin left fringe
@@ -40,6 +41,7 @@
    (right-divider-width . 1)            ;; Thin vertical window divider
    (right-fringe . 8)                   ;; Thin right fringe
    (tool-bar-lines . 0)                 ;; No tool bar
+   (undecorated . t)                    ;; Remove extraneous X decorations
    (vertical-scroll-bars . nil)))       ;; No vertical scroll-bars
 
 ;;; early-init.el ends here
