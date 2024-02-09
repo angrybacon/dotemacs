eab9095a3b51a7525934ad61f5ddcab0cd653ab4
Mathieu Marques <mathieumarques78@gmail.com>
2024-02-10 00:12:47 +0100

    feat: increase window divider width

--- early-init.el
+++ early-init.el
@@ -36,13 +36,13 @@
 (setq-default
  default-frame-alist
  '((background-color . "#3F3F3F")       ; Default background color
-   (bottom-divider-width . 1)           ; Thin horizontal window divider
+   (bottom-divider-width . 4)           ; Thin horizontal window divider
    (foreground-color . "#DCDCCC")       ; Default foreground color
    (fullscreen . maximized)             ; Maximize the window by default
    (horizontal-scroll-bars . nil)       ; No horizontal scroll-bars
    (left-fringe . 8)                    ; Thin left fringe
    (menu-bar-lines . 0)                 ; No menu bar
-   (right-divider-width . 1)            ; Thin vertical window divider
+   (right-divider-width . 4)            ; Thin vertical window divider
    (right-fringe . 8)                   ; Thin right fringe
    (tool-bar-lines . 0)                 ; No tool bar
    (undecorated . t)                    ; Remove extraneous X decorations
