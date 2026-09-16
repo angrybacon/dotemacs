ab82a8704f190d5bdebd1f9cad0b18d81d4684fb
Mathieu Marques <mathieumarques78@gmail.com>
2026-09-17 01:02:35 +0200

    feat(lsp): remove unused inlay configuration

--- lisp/use-lsp.el
+++ lisp/use-lsp.el
@@ -18,32 +18,8 @@ Like `xref-find-references' but switch to the other window."
 
 (declare-function cl-substitute-if "cl-seq")
 (declare-function eglot-current-server "eglot")
-(declare-function eglot-inlay-hints-mode "eglot")
 (declare-function eglot-shutdown "eglot")
 (declare-function me/project-save "use-project")
-
-(defun me/eglot-shutdown-project ()
-  "Kill the LSP server for the current project if it exists."
-  (when-let* ((server (eglot-current-server)))
-    (ignore-errors (eglot-shutdown server))))
-
-(defvar me/eglot-inlay-hints-automatic nil
-  "Whether `eglot-inlay-hints-mode' should be enabled by default.")
-
-(defun me/eglot-inlay-hints-maybe ()
-  "Maybe enable `eglot-inlay-hints-mode'.
-See `me/eglot-inlay-hints-automatic'."
-  (if me/eglot-inlay-hints-automatic
-      (eglot-inlay-hints-mode 1)
-    (eglot-inlay-hints-mode -1)))
-
-(defun me/eglot-inlay-hints-toggle ()
-  "Toggle `me/eglot-inlay-hints-automatic'.
-Also toggle `eglot-inlay-hints-mode' accordingly."
-  (interactive)
-  (let ((value me/eglot-inlay-hints-automatic))
-    (eglot-inlay-hints-mode (if value -1 1))
-    (setq-default me/eglot-inlay-hints-automatic (not value))))
 
 (defun me/eglot-configure-hover ()
   "Configure `eldoc-documentation-functions' to allow multiline entries."
@@ -68,14 +44,6 @@ See https://github.com/typescript-language-server/typescript-language-server."
   (let ((target-modes '(tsx-ts-mode typescript-ts-mode))
         (preferences
          '(:importModuleSpecifierPreference "non-relative"
-           :includeInlayEnumMemberValueHints t
-           :includeInlayFunctionLikeReturnTypeHints t
-           :includeInlayFunctionParameterTypeHints t
-           :includeInlayParameterNameHints "literals" ; none | literals | all
-           :includeInlayParameterNameHintsWhenArgumentMatchesName t
-           :includeInlayPropertyDeclarationTypeHints t
-           :includeInlayVariableTypeHints t
-           :includeInlayVariableTypeHintsWhenTypeMatchesName t
            :organizeImportsCaseFirst "upper"
            :organizeImportsCollation "unicode" ; ordinal | unicode
            :organizeImportsIgnoreCase :json-false
@@ -104,7 +72,6 @@ See https://github.com/typescript-language-server/typescript-language-server."
   (eglot-ignored-server-capabilities '(:semanticTokensProvider))
   :hook
   (eglot-managed-mode . me/eglot-configure-hover)
-  (eglot-managed-mode . me/eglot-inlay-hints-maybe)
   (json-ts-mode . eglot-ensure)
   (python-base-mode . eglot-ensure)
   (scss-mode . eglot-ensure)
